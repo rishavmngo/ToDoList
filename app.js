b2c5f7ac9ca70a5262b217d8da7aa4e8209056c0
Rishav-mngo <rishavinmngo@gmail.com>
2021-05-26 13:11:34 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+var listItems = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    var today = new Date();
+    var options = {
+        weekday: 'long',
+        month: 'long',
+        day:'numeric'
+    }
+    var date_i = today.toLocaleDateString("en-US", options);
+    res.render('list', {dayOfKind: date_i, list_items: listItems});
+});
+
+app.post("/", function(req, res){
+    var item = req.body.inp;
+    listItems.push(item);
+    res.redirect("/");
+    
+
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on 3000");
+});
